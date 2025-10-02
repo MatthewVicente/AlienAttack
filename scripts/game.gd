77e11d892a537260e23f74713c30b71ca1f97cec
Matheus Vicente Rodrigues da Silva <mathewvicente@gmail.com>
2025-10-02 14:34:03 -0300

    Implement GameOver scene and retry button

--- scripts/game.gd
+++ scripts/game.gd
@@ -5,19 +5,28 @@ var score = 0
 
 @onready var player = $Player
 @onready var hud = $UI/HUD
+@onready var ui = $UI
+
+var gos_scene = preload("res://scenes/game_over.tscn")
 
 func _ready():
 	hud._set_score_label(0)
 	hud._set_lives(lives)
 
 func _on_death_zone_area_entered(area):
-	area.die(false)
+	area.queue_free()
 
 func _on_player_took_damage():
 	lives -= 1
 	hud._set_lives(lives)
 	if lives == 0:
 		player.die()
+		
+		await get_tree().create_timer(0.5).timeout
+		
+		var gos = gos_scene.instantiate()
+		gos._set_score(score)
+		ui.add_child(gos)
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance):
 	enemy_instance.connect("enemy_died", _on_enemy_died)
